facfb554ab0c4309b74d50f229aee6a14bcf0dba
孔超 <kongchao592@163.com>
2018-05-04 16:37:43 +0800

    修改程序错误

--- jpa/eclipselink/src/main/java/example/springdata/jpa/eclipselink/Customer.java
+++ jpa/eclipselink/src/main/java/example/springdata/jpa/eclipselink/Customer.java
@@ -36,5 +36,29 @@ import javax.persistence.Id;
 public class Customer {
 
 	private @Id @GeneratedValue Long id;
-	private final String firstname, lastname;
+	private String firstname, lastname;
+  public Customer(String firstname, String lastname) {
+    super();
+    this.firstname = firstname;
+    this.lastname = lastname;
+  }
+  public String getFirstname() {
+    return firstname;
+  }
+  public void setFirstname(String firstname) {
+    this.firstname = firstname;
+  }
+  public String getLastname() {
+    return lastname;
+  }
+  public void setLastname(String lastname) {
+    this.lastname = lastname;
+  }
+  public Long getId() {
+    return id;
+  }
+  public void setId(Long id) {
+    this.id = id;
+  }
+	
 }
